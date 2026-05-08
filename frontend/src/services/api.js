d97fc7f0d381bdc45dd8fc8d212ee3626f15b99e
Касум Микипорис <154760745+Bolkistik@users.noreply.github.com>
2026-05-08 18:51:29 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -11,7 +11,7 @@ const api = axios.create({//создаем axios
 });
 
 api.interceptors.request.use(config => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token')?.trim().replace(/\s/g, '');
     if (token) {
         config.headers.Authorization = `Bearer ${token.trim()}`;
     }
